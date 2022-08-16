6005352ee8ade7e05d1042d299e961bde9f1a0db
Ananya <aahl907@aucklanduni.ac.nz>
2022-08-16 21:19:15 +1200

    Created main game loop and enabled exit feature

--- space-invaders.py
+++ space-invaders.py
@@ -25,3 +25,17 @@ GREEN_LASER = pygame.image.load(os.path.join("assets","laser_green.png"))
 BLUE_LASER = pygame.image.load(os.path.join("assets","laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets","laser_yellow.png"))
 
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    FPS = 60
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            
+            
+main()
