96ebe29b869b9b01a414472bed66fd0ea6549095
Ananya <aahl907@aucklanduni.ac.nz>
2022-08-17 17:56:02 +1200

    Created enemy ships that come down from top of the screen & when they touch bottom then lose point

--- space-invaders.py
+++ space-invaders.py
@@ -56,6 +56,24 @@ class Player(Ship):
         self.mask = pygame.mask.from_surface(self.ship_image)
         self.max_health = health
 
+# Alien ship class that extends Ship
+class Enemy(Ship):
+    
+    # Colour map for association of ship's colour to the image of its laser and itself
+    COLOUR_DICT = {
+        "red": (RED_SHIP, RED_LASER),
+        "green": (GREEN_SHIP, GREEN_LASER),
+        "blue": (BLUE_SHIP, BLUE_LASER)
+    }
+
+
+    def __init__(self, x, y, colour, health=100):
+        super().__init__(x, y, health)
+        self.ship_image, self.laser_image = self.COLOUR_DICT[colour]
+        self.mask = pygame.mask.from_surface(self.ship_image)
+
+    def move(self, speed):
+        self.y += speed
 
 def main():
     
@@ -69,9 +87,15 @@ def main():
     # Speed = How many pixels moved per key press relative to clock speed
     player_speed = 5
 
-    level = 1
+    level = 0
     lives = 5
     
+    # CREATE ENEMIES
+    enemies = []
+    wave_length = 5
+    enemy_speed = 1
+
+
     # CREATE PLAYER
     player = Player(300, 600)
 
@@ -85,6 +109,9 @@ def main():
         WINDOW.blit(lives_label,(10,10))
         WINDOW.blit(level_label,(10, (lives_label.get_height() + 10)))
 
+        for enemy in enemies:
+            enemy.draw(WINDOW)
+
         player.draw(WINDOW)
 
         pygame.display.update()
@@ -92,7 +119,14 @@ def main():
 
     while run:
         clock.tick(FPS)
-        rerender_window()
+        
+        if len(enemies) == 0:
+            level += 1
+            wave_length += 5
+
+            for i in range(wave_length):
+                enemy = Enemy(random.randrange(50, WIDTH-100), random.randrange(-1500, -100), random.choice(["red","blue","green"]))
+                enemies.append(enemy)
 
         for event in pygame.event.get():
 
@@ -110,6 +144,14 @@ def main():
             player.y -= player_speed
         if keys[pygame.K_DOWN] and (player.y + player_speed + player.get_height() < HEIGHT): # moving down using down arrow key
             player.y += player_speed
+
+        for enemy in enemies[:]:
+            enemy.move(enemy_speed)
+            if enemy.y + enemy.get_height() > HEIGHT:
+                lives -= 1
+                enemies.remove(enemy)
+
+        rerender_window()
         
             
 main()
