12c35140615667387726cf189b28b0caeca03df1
Ananya <aahl907@aucklanduni.ac.nz>
2022-08-17 15:26:24 +1200

    Set up background and text (lives etc.) on window

--- space-invaders.py
+++ space-invaders.py
@@ -3,6 +3,7 @@ import pygame
 import os
 import time
 import random
+pygame.font.init()
 
 WIDTH = 700
 HEIGHT = 700
@@ -11,7 +12,7 @@ pygame.display.set_caption("Space Invaders")
 
 # Generate and sync images
 # Background
-BACKGROUND = pygame.image.load(os.path.join("assets","background-space.png"))
+BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("assets","background-space.png")),(WIDTH,HEIGHT)) 
 
 # Ships
 RED_SHIP = pygame.image.load(os.path.join("assets","ship_red.png"))
@@ -27,11 +28,31 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets","laser_yellow.png"))
 
 def main():
     run = True
+    
+    # Set frame speed and clock- fits any device
     clock = pygame.time.Clock()
     FPS = 60
 
+    level = 1
+    lives = 5
+    main_font = pygame.font.SysFont("monospace", 25)
+
+    def rerender_window():
+        WINDOW.blit(BACKGROUND,(0,0))
+        
+        # render and draw labels
+        lives_label = main_font.render(F"LIVES: {lives}",1,(255,255,255))
+        level_label = main_font.render(F"LEVEL: {level}",1,(255,255,255))
+
+        WINDOW.blit(lives_label,(10,10))
+        WINDOW.blit(level_label,(10, (lives_label.get_height() + 10)))
+
+        pygame.display.update()
+
+
     while run:
         clock.tick(FPS)
+        rerender_window()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
