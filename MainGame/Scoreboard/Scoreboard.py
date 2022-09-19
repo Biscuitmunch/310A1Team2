b4284269a5b760c13e76559566981497057710a6
BreannaJury <79810883+bjur781@users.noreply.github.com>
2022-09-20 09:45:22 +1200

    feat/updated scoreboard UI

--- MainGame/Scoreboard/Scoreboard.py
+++ MainGame/Scoreboard/Scoreboard.py
@@ -1,4 +1,5 @@
 import pygame
+import Buttons.InstrucButton as Button
 
 #font
 pygame.font.init()
@@ -15,47 +16,59 @@ PONG_HIGHSCORE_FILE_PATH = 'MainGame/Pong/pongScore.txt'
 # Set window title
 pygame.display.set_caption('Snake')
 background_color = (255, 255, 255)
-font = pygame.font.Font('MainGame/Scoreboard/resources/Roboto_MediumItalic.ttf', 45)
 
 def startScoreboard():
     run = True
     scores_displayed = False
     screen.fill((0, 0, 0))
+
+    
+
     while run == True:
+        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+        
+        button_font = pygame.font.Font("MainGame/Buttons/PressStart2P.ttf", 35)
+        title_font = pygame.font.Font("MainGame/Buttons/PressStart2P.ttf", 50)
+        font = pygame.font.Font("MainGame/Buttons/PressStart2P.ttf", 25)
+
+        QUIT_BUTTON = Button.Button(image=None, pos=(125, 650), text_input="MENU", font=button_font, base_color="White", hovering_color="Green")
+        QUIT_BUTTON.changeColor(PLAY_MOUSE_POS)
+        QUIT_BUTTON.update(screen)
+
+        TITLE_TEXT = title_font.render("Scoreboard", True, "White")
+        TITLE_RECT = TITLE_TEXT.get_rect(center=(300, 70))
+        screen.blit(TITLE_TEXT, TITLE_RECT)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                run = False
+            elif QUIT_BUTTON.checkForInput(PLAY_MOUSE_POS) and event.type == pygame.MOUSEBUTTONDOWN:
                 run = False
 
             elif scores_displayed == False:
                 with open(SNAKE_HIGHSCORE_FILE_PATH, "r") as file:
                     line = file.readline()  # scores are one line
                     text = font.render("Snake High Score: " + line, True, (WHITE))
-                    screen.blit(text, [50, 135])
+                    screen.blit(text, [50, 140])
 
                 with open(INVADER_HIGHSCORE_FILE_PATH, "r") as file:
                     line = file.readline()  # scores are one line
                     text = font.render("Space Invaders High Score: " + line, True, (WHITE))
-                    screen.blit(text, [50, 235])
+                    screen.blit(text, [50, 240])
 
                 with open(ASTEROIDS_HIGHSCORE_FILE_PATH, "r") as file:
                     line = file.readline()  # scores are one line
                     text = font.render("Asteroids High Score: " + line, True, (WHITE))
-                    screen.blit(text, [50, 335])
+                    screen.blit(text, [50, 340])
 
                 with open(PONG_HIGHSCORE_FILE_PATH, "r") as file:
                     line = file.readline()  # scores are one line
                     text = font.render("Pong High Score: " + line, True, (WHITE))
-                    screen.blit(text, [50, 435])
+                    screen.blit(text, [50, 440])
 
                 scores_displayed = True
 
-                
-
-
-
-
-
+            pygame.display.update()
             pygame.display.flip()
             clock.tick(60)
 
