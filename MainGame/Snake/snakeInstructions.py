bdcaf80cf7950fb87ad49ebc573d8dad25679d53
BreannaJury <79810883+bjur781@users.noreply.github.com>
2022-09-19 13:20:14 +1200

    feat/completed snake instructions

--- MainGame/Snake/snakeInstructions.py
+++ MainGame/Snake/snakeInstructions.py
@@ -1,6 +1,7 @@
 import pygame
 import Snake.snake as snake
 import sys
+import Buttons.InstrucButton as Button
 
 WIDTH = 1280
 HEIGHT = 720
@@ -22,57 +23,53 @@ class ScreenItem():
             if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
                 return True
 
+
 class instructions:
 
     def play(self):
-        while True:            
+        running = True
+        while running:            
             window = pygame.display.set_mode((WIDTH, HEIGHT))
             window.fill("black")
-            font = pygame.font.SysFont('arial', 25)
+            PLAY_MOUSE_POS = pygame.mouse.get_pos()
 
-            play_image_un = pygame.image.load(
-                "MainGame/Buttons/PlayUnpressedButton.png").convert_alpha()
-            play_image_pressed = pygame.image.load(
-                "MainGame/Buttons/PlayPressedButton.png").convert_alpha()
-            quit_image_un = pygame.image.load(
-                "MainGame/Buttons/QuitUnpressedButton.png").convert_alpha()
-            quit_image_pressed = pygame.image.load(
-                "MainGame/Buttons/QuitPressedButton.png").convert_alpha()
+            font = pygame.font.Font("MainGame/Buttons/PressStart2P.ttf", 25)
+            title_font = pygame.font.Font("MainGame/Buttons/PressStart2P.ttf", 50)
+            button_font = pygame.font.Font("MainGame/Buttons/PressStart2P.ttf", 35)
 
-            play = ScreenItem(0, 0, play_image_un)
-            quit = ScreenItem(WIDTH - 100, HEIGHT - 100, quit_image_un)
+            # Game title
+            PLAY_TEXT = title_font.render("SNAKE", True, "White")
+            PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 200))
+            window.blit(PLAY_TEXT, PLAY_RECT)
 
+            # Instructions (3 lines)
+            INSTRUC_TEXT_1 = font.render("use arrow keys to change directions", True, "White")
+            INSTRUC_RECT_1 = INSTRUC_TEXT_1.get_rect(center=(640, 300))
+            window.blit(INSTRUC_TEXT_1, INSTRUC_RECT_1)
+            INSTRUC_TEXT_2 = font.render("don’t run into any walls or your own tail", True, "White")
+            INSTRUC_RECT_2 = INSTRUC_TEXT_2.get_rect(center=(640, 350))
+            window.blit(INSTRUC_TEXT_2, INSTRUC_RECT_2)
+            INSTRUC_TEXT_3 = font.render("eat cherries to grow", True, "White")
+            INSTRUC_RECT_3 = INSTRUC_TEXT_3.get_rect(center=(640, 400))
+            window.blit(INSTRUC_TEXT_3, INSTRUC_RECT_3)
+
+            # Navigation buttons
+            PLAY_BUTTON = Button.Button(image=None, pos=(900, 550), text_input="PLAY", font=button_font, base_color="White", hovering_color="Green")
+            PLAY_BUTTON.changeColor(PLAY_MOUSE_POS)
+            PLAY_BUTTON.update(window)
+            QUIT_BUTTON = Button.Button(image=None, pos=(380, 550), text_input="MENU", font=button_font, base_color="White", hovering_color="Green")
+            QUIT_BUTTON.changeColor(PLAY_MOUSE_POS)
+            QUIT_BUTTON.update(window)
+
+            # Check for clicks
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-
-            for event in pygame.event.get():
-                
-                # User tried to click:
+                    running = False
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    if play.mouse_over_button(pygame.mouse.get_pos()):
+                    if PLAY_BUTTON.checkForInput(PLAY_MOUSE_POS):
                         snake_obj = snake.snake_game()
                         snake_obj.start_game()
-                        
-                    elif quit.mouse_over_button(pygame.mouse.get_pos()):
-                        pygame.quit()
-                        sys.exit()
-                    
+                    elif QUIT_BUTTON.checkForInput(PLAY_MOUSE_POS):
+                        running = False
 
-                #Detect mouse hovering
-                if event.type == pygame.MOUSEMOTION:
-                    # Activate Snake
-                    if play.mouse_over_button(pygame.mouse.get_pos()):
-                        play = ScreenItem(WIDTH/2, HEIGHT/2, play_image_pressed)
-                    
-                    #Activate Avatar Select
-                    elif quit.mouse_over_button(pygame.mouse.get_pos()):
-                        quit = ScreenItem(WIDTH/2, HEIGHT/2, quit_image_pressed)
-
-
-            # Show buttons on screen
-            play.update()
-            quit.update()
-            
             pygame.display.update()
