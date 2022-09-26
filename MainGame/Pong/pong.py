cecab0d464579ab02a7c12e110aa3e69339c7e4d
HZ991 <79825123+HZ991@users.noreply.github.com>
2022-09-27 05:28:03 +1300

    feat: implemented game over screen for pong

--- MainGame/Pong/pong.py
+++ MainGame/Pong/pong.py
@@ -2,6 +2,7 @@ from os import environ
 environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import pygame
 import random
+import Gameover
 
 pygame.init()
 
@@ -152,6 +153,11 @@ class PongGame:
                 set_high_score(self.score_player)
                 game_over = True
                 break_loops = True
+
+                # goes to game over screen
+                pygame.display.set_caption("Game Over")
+                Gameover.gameover().gameOver("pong")
+
                 pygame.display.set_caption("Arcade Menu")
 
             # Updating the screen on each loop, keeping assets updated
